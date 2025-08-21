cb6b031878c55966cc1639027eec11ba0b9d17ea
Rene Senoner <Senonerrene@gmail.com>
2025-08-21 19:33:03 +0000

    und auch noch damit ichd as provate schlüssen eingeben jann also das inh

--- src/lib/firebase-admin.ts
+++ src/lib/firebase-admin.ts
@@ -1,18 +1,24 @@
 // src/lib/firebase-admin.ts
 import admin from 'firebase-admin';
 
-// This is a placeholder for your service account credentials.
 // In a real application, you would load this from a secure location (e.g., environment variables).
 const serviceAccount = {
-  // Your service account key here
-  // "type": "service_account",
-  // "project_id": "...",
-  // ...
-};
+  type: process.env.FIREBASE_ADMIN_TYPE,
+  project_id: process.env.FIREBASE_ADMIN_PROJECT_ID,
+  private_key_id: process.env.FIREBASE_ADMIN_PRIVATE_KEY_ID,
+  private_key: process.env.FIREBASE_ADMIN_PRIVATE_KEY?.replace(/\\n/g, '\n'),
+  client_email: process.env.FIREBASE_ADMIN_CLIENT_EMAIL,
+  client_id: process.env.FIREBASE_ADMIN_CLIENT_ID,
+  auth_uri: process.env.FIREBASE_ADMIN_AUTH_URI,
+  token_uri: process.env.FIREBASE_ADMIN_TOKEN_URI,
+  auth_provider_x509_cert_url: process.env.FIREBASE_ADMIN_AUTH_PROVIDER_X509_CERT_URL,
+  client_x509_cert_url: process.env.FIREBASE_ADMIN_CLIENT_X509_CERT_URL,
+}
 
 if (!admin.apps.length) {
   try {
     admin.initializeApp({
+      // @ts-ignore
       credential: admin.credential.cert(serviceAccount),
     });
   } catch (error) {
